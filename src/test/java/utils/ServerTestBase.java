a74edbe8d56afc7787c55d77f4743c453017fc91
Clement <clement.jean@epitech.eu>
2022-03-11 11:58:30 +0800

    Supress warning for raw type use

--- src/test/java/utils/ServerTestBase.java
+++ src/test/java/utils/ServerTestBase.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+@SuppressWarnings("rawtypes")
 public class ServerTestBase<BLOCKING_STUB extends AbstractBlockingStub, ASYNC_STUB extends AbstractAsyncStub> {
     private static final String serverName = "test";
 
@@ -24,7 +25,7 @@ public class ServerTestBase<BLOCKING_STUB extends AbstractBlockingStub, ASYNC_ST
     protected BLOCKING_STUB blockingStub = null;
     protected ASYNC_STUB asyncStub = null;
 
-    private final List<BindableService> services = new ArrayList<BindableService>();
+    private final List<BindableService> services = new ArrayList<>();
 
     protected void addService(BindableService service) {
         this.services.add(service);
--- src/test/java/utils/StubInstantiator.java
+++ src/test/java/utils/StubInstantiator.java
@@ -3,6 +3,7 @@ package utils;
 import io.grpc.ManagedChannel;
 import io.grpc.stub.AbstractStub;
 
+@SuppressWarnings("rawtypes")
 public interface StubInstantiator<STUB extends AbstractStub> {
     STUB instantiate(ManagedChannel channel);
 }
